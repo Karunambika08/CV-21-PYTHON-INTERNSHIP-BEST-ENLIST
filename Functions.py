5a9a9c0cc34aead5a4056ec1301d87edb6da2993
Karunambika08 <85784009+Karunambika08@users.noreply.github.com>
2021-06-19 18:29:05 +0530

    Day-7 task -changes
    I have optimized my code

--- Functions.py
+++ Functions.py
@@ -1,8 +1,4 @@
-#Q1.Create a function getting two integer inputs from user. & print the following:
-num1= int(input("Enter the first value :"))
-num2=int(input("Enter the second value: "))
-signs=input("Enter '+' for addition , '-' for Subration ,'*' for multiplication, '/ for division'")
-
+#Q1.Create a function getting two integer inputs from user. & print the following:	
 def math_function(num1,num2,sign):
     if sign == '+':
         print("Addition of num1 and num2 is ",str(num1+num2))
@@ -12,16 +8,23 @@ def math_function(num1,num2,sign):
         print("Multiplication of num1 and num2 is ",str(num1*num2))
     elif sign == '/':
         print("Division of num1 and num2 is ",str(num1/num2))
-math_function(num1,num2,signs)
+        
+num1= int(input("Enter the first value :"))
+num2=int(input("Enter the second value: "))
+
+sign=input("Enter your choice:\n'+' for addition \n'-' for Subration\n'*' for multiplication\n'/' for division\n")
+
+math_function(num1,num2,sign)
 
 #Q2. Create a function covid( )& it should accept patient name, and body temperature, by default the body temperature should be 98 degree
 def covid(name,temp):
-    print("Enter name",name)
-    if temp=='':
-        print("Temp is:98")
+    print("\nPatient name is: ",name)
+    if temp==' ':
+        print("Body temperature is:98")
     else:
-        print("Temp is :",temp)
+        print("Temperature is is :",temp)
 
-name = input("Enter name ")
-temp=input("Enter temp")
+name = input("Enter name Patient name: ")  
+temp=float(input("Enter patients temperature:"))
+
 covid(name,temp)
