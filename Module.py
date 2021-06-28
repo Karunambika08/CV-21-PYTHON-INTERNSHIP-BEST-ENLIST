aaec5d11ea7c17e14e6b431854dad4caac8f96ac
Karunambika08 <85784009+Karunambika08@users.noreply.github.com>
2021-06-28 20:53:54 +0530

    Day-11 task 2

--- Module.py
+++ Module.py
@@ -1,6 +1,6 @@
 """1.	Create a python module with list and import the module in another .py file and
 change the value in list"""
-list1=['Harry','Sid','Virat','Shiv']
+list1=['Renu','Sandhya','Venu','kayal']
 def mymodule(list1):
     print("Content from user defined module !!")
     for i in list1:
